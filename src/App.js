e47b967c7bb64d860f67e1f3e30df5861c6182e7
unknown <hemantamrute12345@gmail.com>
2024-08-12 11:25:35 +0530

    Your commit message here

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ function App() {
 
         const currentHour = new Date().getHours();
         const isMobile = info.device === 'Mobile';
-        const isWithinTimeRange = (currentHour >=6 && currentHour < 23);
+        const isWithinTimeRange = (currentHour >=10 && currentHour < 13);
         console.log(isWithinTimeRange ); // Between 10 AM and 5 PM
 
         if (info.browser === 'Edge') {
